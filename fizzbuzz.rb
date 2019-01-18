66ae044056fee910d3d2a98b7516cc4d112f6719
Ming Duan <11203duan@gmail.com>
2019-01-18 23:32:33 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
+def fizzbuzz(input)
+  if input % 3 == 0 && input % 5 != 0
+    return "Fizz"
+  elsif input % 5 == 0 && input % 3 != 0
+    return "Buzz"
+  elsif input % 15 == 0
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
