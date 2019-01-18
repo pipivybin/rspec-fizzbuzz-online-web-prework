7f513efdee039454778d43efd9041c82bf7dd00f
Ming Duan <11203duan@gmail.com>
2019-01-18 23:16:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
+def fizzbuzz(input)
+  if input == 3
+    return "Fizz"
+  end
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
